628858fc29ffcc319d43b78c6ebec1807868b277
Vinicius <vinnii@DESKTOP-P8C91NB.localdomain>
2020-06-19 20:26:27 -0300

    Audio

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import React, { useState } from 'react';
+import React, { useRef } from 'react';
 import './App.css';
 import Routes from './routes';
+import { useEffect } from 'react';
 import url from './assets/audio/song.mp3';
-
 function App() {
-  const [audio] = useState(new Audio(url));
+  const audioRef = useRef();
 
-  audio.play();
-  audio.volume = 0.05;
-  audio.loop = 1;
+  useEffect(() => {
+    if (audioRef.current) {
+      audioRef.current.volume = 0.15;
+      audioRef.current.muted = false;
+      setTimeout(() => {
+        audioRef.current.play();
+      }, 2000);
+    }
+  }, [audioRef]);
 
+  document.body.addEventListener('mousemove', function () {
+    audioRef.current.play();
+  });
   return (
     <div className='App'>
+      <audio src={url} ref={audioRef} muted></audio>
       <Routes />
     </div>
   );
